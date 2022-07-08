acfc9d62307c9edfd83f0ed654adf4c4a7c26cf3
s-ferrer <saraferrer@gmail.com>
2022-07-08 16:30:13 +0200

    Create folder structure

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatLANDPAGE,
+  Header,
+} from "./containers";
+
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatLANDPAGE />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/whatLANDPAGE/WhatLANDPAGE.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatLANDPAGE.css";
+
+const WhatLANDPAGE = () => {
+  return <div>WhatLANDPAGE</div>;
+};
+
+export default WhatLANDPAGE;

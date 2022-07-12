6e195399e6dcebbbc53c8bfc30861a80fce13de7
s-ferrer <saraferrer@gmail.com>
2022-07-12 12:21:18 +0200

    Style Feature.jsx component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,12 +3,12 @@ import "./feature.css";
 
 const Feature = ({ title, text }) => {
   return (
-    <div className="gpt3__whatgpt3-features-container__feature">
-      <div className="gpt3__whatgpt3-features-container__feature-text">
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
         <div />
         <h1>{title}</h1>
       </div>
-      <div className="gpt3__whatgpt3-features-container_feature-text">
+      <div className="gpt3__features-container_feature-text">
         <p>{text}</p>
       </div>
     </div>

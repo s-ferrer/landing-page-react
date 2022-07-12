2a31c2853d68451e49bbbb0a33a4a7cd91fe030b
s-ferrer <saraferrer@gmail.com>
2022-07-12 13:17:48 +0200

    Style Possibility component in css file

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import possibilityImage from "../../assets/possibility.png";
 import "./possibility.css";
 
 const Possibility = () => {
   return (
     <div className="gpt3__possibility section__padding" id="possibility">
       <div className="gpt3__possibility-image">
-        <img src="{possibilityImage}" alt="possibility" />
+        <img src={possibilityImage} alt="possibility" />
       </div>
       <div className="gpt3__possibility-content">
         <h4>Request Early Access to Get Started</h4>
